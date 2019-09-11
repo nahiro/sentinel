f3c5230ee202669cbe897af21989e958196b8b48
Naohiro MANAGO <manago.naohiro@gmail.com>
2019-09-11 17:45:11 +0900

    NM modified to improve output figure.

--- find_peaks.py
+++ find_peaks.py
@@ -3,7 +3,7 @@ import os
 import sys
 import re
 from glob import glob
-from datetime import datetime
+from datetime import datetime,timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -12,26 +12,41 @@ from matplotlib.backends.backend_pdf import PdfPages
 from optparse import OptionParser,IndentedHelpFormatter
 
 # Default values
+END = datetime.now().strftime('%Y%m%d')
+PERIOD = 90 # day
 XSGM = 5.0 # day
 STHR = 1.0
 NTHR = 1
 BTHR = 1.5 # dB
 YTHR = 2.0
 DTHR = 0.05
+DATDIR = '.'
+OUTNAM = 'find_peaks.dat'
+FIGNAM = 'find_peaks.pdf'
 
 # Read options
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
 parser.set_usage('Usage: %prog collocated_geotiff_file [options]')
+parser.add_option('-e','--end',default=END,help='End date of the analysis in the format YYYYMMDD (%default)')
+parser.add_option('-p','--period',default=PERIOD,type='int',help='Observation period in day (%default)')
 parser.add_option('-w','--xsgm',default=XSGM,type='float',help='Standard deviation of gaussian in day (%default)')
 parser.add_option('-s','--sthr',default=STHR,type='float',help='Threshold of bg-subtracted signal in dB (%default)')
 parser.add_option('-n','--nthr',default=NTHR,type='int',help='Minimum number of bg-subtracted signal above threshold (%default)')
 parser.add_option('-b','--bthr',default=BTHR,type='float',help='Background level above average in dB (%default)')
 parser.add_option('-y','--ythr',default=YTHR,type='float',help='Threshold of superposed gaussians for peak search (%default)')
-parser.add_option('-d','--dthr',default=DTHR,type='float',help='Threshold of dy/dt for local maximum search (%default)')
+parser.add_option('--dthr',default=DTHR,type='float',help='Threshold of dy/dt for local maximum search (%default)')
+parser.add_option('-D','--datdir',default=DATDIR,help='Input data directory (%default)')
+parser.add_option('-o','--outnam',default=OUTNAM,help='Output data name (%default)')
+parser.add_option('-F','--fignam',default=FIGNAM,help='Output figure name for debug (%default)')
+parser.add_option('--ax1_ymax',default=None,type='float',help='Axis1 Y max. (%default)')
+parser.add_option('--ax1_ymin',default=None,type='float',help='Axis1 Y min. (%default)')
+parser.add_option('--ax1_ystp',default=None,type='float',help='Axis1 Y step. (%default)')
+parser.add_option('-d','--debug',default=False,action='store_true',help='Debug mode (%default)')
 (opts,args) = parser.parse_args()
+d1 = datetime.strptime(opts.end,'%Y%m%d')
+d0 = d1-timedelta(days=opts.period)
 
-datdir = '../190906'
-fs = sorted(glob(os.path.join(datdir,'collocation_*.dat')))
+fs = sorted(glob(os.path.join(opts.datdir,'collocation_*.dat')))
 
 xmin = 1.0e10
 xmax = -1.0e10
@@ -43,8 +58,10 @@ for i,f in enumerate(fs):
     m = re.search('collocation_(\d+)_(\d+).dat',os.path.basename(f))
     if not m:
         continue
-    d0 = date2num(datetime.strptime(m.group(1),'%y%m%d'))
-    d1 = date2num(datetime.strptime(m.group(2),'%y%m%d'))
+    d0_tmp = datetime.strptime(m.group(1),'%y%m%d')
+    d1_tmp = datetime.strptime(m.group(2),'%y%m%d')
+    if d1_tmp < d0 or d1_tmp > d1:
+        continue
     try:
         j,ndat,tmin,vmin,fmin,tlft,vlft,flft,trgt,vrgt,frgt,dmin,dstd,tleg,fleg,treg,freg,sstd,scor,traw,vraw,fraw,draw,rstd,rcor,bavg = np.loadtxt(f,unpack=True)
     except Exception:
@@ -55,43 +72,61 @@ for i,f in enumerate(fs):
         if not np.all(j == jdat):
             raise ValueError('Error, different j')
     tdat.append(tmin)
-    ydat.append(d1)
+    ydat.append(d1_tmp)
     sdat.append(bavg-vmin)
-    if d0 < xmin:
-        xmin = d0
-    if d1 > xmax:
-        xmax = d1
+    x0_tmp = date2num(d0_tmp)
+    x1_tmp = date2num(d1_tmp)
+    if x0_tmp < xmin:
+        xmin = x0_tmp
+    if x1_tmp > xmax:
+        xmax = x1_tmp
 jdat = np.array(jdat)
 tdat = np.array(tdat)
 ydat = np.array(ydat)
 sdat = np.array(sdat)
 
-plt.interactive(True)
-fig = plt.figure(1,facecolor='w',figsize=(6,3.5))
-pdf = PdfPages('find_peaks.pdf')
-
-values = []
-labels = []
-ticks = []
-for y in [2017,2018,2019]:
-    for m in range(1,13,3):
-        d = datetime(y,m,1)
-        values.append(date2num(d))
-        labels.append(d.strftime('%Y-%m'))
-    for m in range(1,13,1):
-        d = datetime(y,m,1)
-        ticks.append(date2num(d))
+if opts.debug:
+    plt.interactive(True)
+    fig = plt.figure(1,facecolor='w',figsize=(6,3.5))
+    pdf = PdfPages(opts.fignam)
+    values = []
+    labels = []
+    ticks = []
+    ds = (xmax-xmin)/365
+    for y in range(num2date(xmin).year,num2date(xmax).year+1):
+        if ds > 2.0:
+            for m in range(1,13,3):
+                d = datetime(y,m,1)
+                values.append(date2num(d))
+                labels.append(d.strftime('%Y-%m'))
+            for m in range(1,13,1):
+                d = datetime(y,m,1)
+                ticks.append(date2num(d))
+        elif ds > 1.0:
+            for m in range(1,13,2):
+                d = datetime(y,m,1)
+                values.append(date2num(d))
+                labels.append(d.strftime('%Y-%m'))
+            for m in range(1,13,1):
+                d = datetime(y,m,1)
+                ticks.append(date2num(d))
+        else:
+            for m in range(1,13,1):
+                d = datetime(y,m,1)
+                values.append(date2num(d))
+                labels.append(d.strftime('%Y-%m'))
 
 xval = np.arange(xmin,xmax,0.1)
 dx_1 = 1.0/np.gradient(xval)
 for sid in range(jdat.size):
     if sid%100 == 0:
         sys.stderr.write('{}\n'.format(sid))
-    fig.clear()
-    plt.subplots_adjust(top=0.85,bottom=0.20,left=0.15,right=0.90)
-    ax1 = plt.subplot(111)
-    ax1.minorticks_on()
-    ax1.grid(True)
+    if opts.debug:
+        fig.clear()
+        plt.subplots_adjust(top=0.85,bottom=0.20,left=0.15,right=0.90)
+        ax1 = plt.subplot(111)
+        ax1.minorticks_on()
+        ax1.grid(True)
     cnd = np.abs(jdat-sid) < 1.0e-4
     tt = tdat[:,cnd].reshape(ydat.shape)
     ss = sdat[:,cnd].reshape(ydat.shape)
@@ -118,7 +153,6 @@ for sid in range(jdat.size):
                 eind.append(ind1[itmp])
                 sind.append(ind1[itmp+1])
         eind.append(ind1[-1])
-
     for si,ei in zip(sind,eind):
         cnd3 = (tt >= xval[si]) & (tt <= xval[ei]) & (ysig > opts.sthr)
         if cnd3.sum() < opts.nthr:
@@ -136,21 +170,28 @@ for sid in range(jdat.size):
                 ind6 = ind0[ind3][np.argmax(yval[ind3])]
         else:
             ind6 = ind0[ind3][np.argmax(yval[ind3])]
-        #ax1.plot(xval[ind3],yval[ind3],'c-',zorder=10)
-        ax1.hlines(opts.ythr,xval[si],xval[ei],zorder=10)
-        ax1.plot(xval[ind6],yval[ind6],'ro',zorder=10)
-    ax1.plot(xval,yval,'b-',zorder=1)
-    ax1.set_ylabel('Signal value (dB)')
-    ax1.set_xlim(xmin,xmax)
-    ax1.set_ylim(0.0,50.0)
-    ax1.set_title('#{} ({} - {})'.format(sid,num2date(xmin).strftime('%Y-%m-%d'),num2date(xmax).strftime('%Y-%m-%d')))
-    ax1.xaxis.set_major_locator(plt.FixedLocator(values))
-    ax1.xaxis.set_major_formatter(plt.FixedFormatter(labels))
-    ax1.xaxis.set_minor_locator(plt.FixedLocator(ticks))
-    ax1.xaxis.set_tick_params(pad=7)
-    ax1.yaxis.set_label_coords(-0.10,0.5)
-    fig.autofmt_xdate()
-    plt.draw()
-    plt.savefig(pdf,format='pdf')
-    #break
-pdf.close()
+        if opts.debug:
+            ax1.hlines(opts.ythr,xval[si],xval[ei],zorder=10)
+            ax1.plot(xval[ind6],yval[ind6],'ro',zorder=10)
+    if opts.debug:
+        ax1.plot(xval,yval,'b-',zorder=1)
+        ax1.set_ylabel('Signal value (dB)')
+        ax1.set_xlim(xmin,xmax)
+        if opts.ax1_ymin is not None:
+            ax1.set_ylim(bottom=opts.ax1_ymin)
+        if opts.ax1_ymax is not None:
+            ax1.set_ylim(top=opts.ax1_ymax)
+        if opts.ax1_ystp is not None:
+            ax1.yaxis.set_major_locator(plt.MultipleLocator(opts.ax1_ystp))
+        ax1.set_title('#{} ({} - {})'.format(sid,num2date(xmin).strftime('%Y-%m-%d'),num2date(xmax).strftime('%Y-%m-%d')))
+        ax1.xaxis.set_major_locator(plt.FixedLocator(values))
+        ax1.xaxis.set_major_formatter(plt.FixedFormatter(labels))
+        ax1.xaxis.set_minor_locator(plt.FixedLocator(ticks))
+        ax1.xaxis.set_tick_params(pad=7)
+        ax1.yaxis.set_label_coords(-0.10,0.5)
+        fig.autofmt_xdate()
+        plt.draw()
+        plt.savefig(pdf,format='pdf')
+    break
+if opts.debug:
+    pdf.close()
