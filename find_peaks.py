23ac6f1fa6abe9cbf104fb6a313008411d7a86ce
Naohiro MANAGO <manago.naohiro@chiba-u.jp>
2019-09-18 11:52:34 +0900

    NM modified to output subpeaks.

--- find_peaks.py
+++ find_peaks.py
@@ -15,11 +15,12 @@ from optparse import OptionParser,IndentedHelpFormatter
 END = datetime.now().strftime('%Y%m%d')
 PERIOD = 90 # day
 XSGM = 5.0 # day
-STHR = 1.0
+STHR = 0.5 # dB
 NTHR = 1
 BTHR = 1.5 # dB
-YTHR = 2.0
-DTHR = 0.05
+YTHR = 1.0 # dB
+DTHR = 0.05 # dB/day
+VINT = 100
 DATDIR = '.'
 OUTNAM = 'find_peaks.dat'
 FIGNAM = 'find_peaks.pdf'
@@ -28,26 +29,33 @@ FIGNAM = 'find_peaks.pdf'
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
 parser.set_usage('Usage: %prog collocated_geotiff_file [options]')
 parser.add_option('-e','--end',default=END,help='End date of the analysis in the format YYYYMMDD (%default)')
+parser.add_option('-E','--enum',default=None,type='int',help='End number (%default)')
 parser.add_option('-p','--period',default=PERIOD,type='int',help='Observation period in day (%default)')
 parser.add_option('-w','--xsgm',default=XSGM,type='float',help='Standard deviation of gaussian in day (%default)')
 parser.add_option('-s','--sthr',default=STHR,type='float',help='Threshold of bg-subtracted signal in dB (%default)')
 parser.add_option('-n','--nthr',default=NTHR,type='int',help='Minimum number of bg-subtracted signal above threshold (%default)')
 parser.add_option('-b','--bthr',default=BTHR,type='float',help='Background level above average in dB (%default)')
-parser.add_option('-y','--ythr',default=YTHR,type='float',help='Threshold of superposed gaussians for peak search (%default)')
-parser.add_option('--dthr',default=DTHR,type='float',help='Threshold of dy/dt for local maximum search (%default)')
+parser.add_option('-y','--ythr',default=YTHR,type='float',help='Threshold of superposed gaussians in dB for peak search (%default)')
+parser.add_option('--dthr',default=DTHR,type='float',help='Threshold of dy/dt in dB/day for local maximum search (%default)')
 parser.add_option('-D','--datdir',default=DATDIR,help='Input data directory (%default)')
 parser.add_option('-o','--outnam',default=OUTNAM,help='Output data name (%default)')
 parser.add_option('-F','--fignam',default=FIGNAM,help='Output figure name for debug (%default)')
 parser.add_option('--ax1_ymax',default=None,type='float',help='Axis1 Y max. (%default)')
 parser.add_option('--ax1_ymin',default=None,type='float',help='Axis1 Y min. (%default)')
 parser.add_option('--ax1_ystp',default=None,type='float',help='Axis1 Y step. (%default)')
+parser.add_option('-S','--subpeak',default=False,action='store_true',help='Output sub peaks (%default)')
+parser.add_option('-z','--npz',default=False,action='store_true',help='NPZ mode (%default)')
+parser.add_option('--vint',default=VINT,type='int',help='Verbose output interval (%default)')
 parser.add_option('-v','--verbose',default=False,action='store_true',help='Verbose mode (%default)')
 parser.add_option('-d','--debug',default=False,action='store_true',help='Debug mode (%default)')
 (opts,args) = parser.parse_args()
 d1 = datetime.strptime(opts.end,'%Y%m%d')
 d0 = d1-timedelta(days=opts.period)
 
-fs = sorted(glob(os.path.join(opts.datdir,'collocation_*.dat')))
+if opts.npz:
+    fs = sorted(glob(os.path.join(opts.datdir,'collocation_*.npz')))
+else:
+    fs = sorted(glob(os.path.join(opts.datdir,'collocation_*.dat')))
 
 xmin = 1.0e10
 xmax = -1.0e10
@@ -56,7 +64,10 @@ tdat = []
 ydat = []
 sdat = []
 for i,f in enumerate(fs):
-    m = re.search('collocation_(\d+)_(\d+).dat',os.path.basename(f))
+    if opts.npz:
+        m = re.search('collocation_(\d+)_(\d+).npz',os.path.basename(f))
+    else:
+        m = re.search('collocation_(\d+)_(\d+).dat',os.path.basename(f))
     if not m:
         continue
     d0_tmp = datetime.strptime(m.group(1),'%y%m%d')
@@ -64,7 +75,14 @@ for i,f in enumerate(fs):
     if d1_tmp < d0 or d1_tmp > d1:
         continue
     try:
-        j,ndat,tmin,vmin,fmin,tlft,vlft,flft,trgt,vrgt,frgt,dmin,dstd,tleg,fleg,treg,freg,sstd,scor,traw,vraw,fraw,draw,rstd,rcor,bavg = np.loadtxt(f,unpack=True)
+        if opts.npz:
+            data = np.load(f)
+            j = data['j']
+            tmin = data['tmin']
+            vmin = data['vmin']
+            bavg = data['bavg']
+        else:
+            j,ndat,tmin,vmin,fmin,tlft,vlft,flft,trgt,vrgt,frgt,dmin,dstd,tleg,fleg,treg,freg,sstd,scor,traw,vraw,fraw,draw,rstd,rcor,bavg = np.loadtxt(f,unpack=True)
     except Exception:
         continue
     if jdat is None:
@@ -121,7 +139,7 @@ xval = np.arange(xmin,xmax,0.1)
 dx_1 = 1.0/np.gradient(xval)
 with open(opts.outnam,'w') as fp:
     for sid in range(jdat.size):
-        if opts.verbose and sid%100 == 0:
+        if opts.verbose and sid%opts.vint == 0:
             sys.stderr.write('{}\n'.format(sid))
         if opts.debug:
             fig.clear()
@@ -161,23 +179,47 @@ with open(opts.outnam,'w') as fp:
                 continue
             ind3 = ind0[si:ei+1]
             ind4 = np.where((np.abs(y_1d[ind3]) < opts.dthr) & (y_2d[ind3] < 0.0))[0]
-            if ind4.size > 0:
+            if ind4.size > 0: # convex
                 dind = np.diff(ind4)
                 ind5 = np.where(dind > 1)[0]
-                if ind5.size > 0:
-                    sj = ind4[0]
-                    ej = ind4[ind5[0]]
-                    ind6 = ind3[sj:ej+1][np.nanargmax(yval[ind3][sj:ej+1])]
-                else:
+                if ind5.size > 0: # multiple peak
+                    if opts.subpeak:
+                        sjs = []
+                        ejs = []
+                        sjs.append(ind4[0])
+                        for jtmp in ind5:
+                            ejs.append(ind4[jtmp])
+                            sjs.append(ind4[jtmp+1])
+                        ejs.append(ind4[-1])
+                        xpks = []
+                        ypks = []
+                        for sj,ej in zip(sjs,ejs):
+                            ind6 = ind3[sj:ej+1][np.nanargmax(yval[ind3][sj:ej+1])] # the first peak
+                            xpks.append(xval[ind6])
+                            ypks.append(yval[ind6])
+                    else:
+                        sj = ind4[0] # the first peak
+                        ej = ind4[ind5[0]] # the first peak
+                        ind6 = ind3[sj:ej+1][np.nanargmax(yval[ind3][sj:ej+1])] # the first peak
+                        xpks = [xval[ind6]]
+                        ypks = [np.nanmax(yval[ind3])] # may not equal to yval[ind6]
+                else: # single peak
                     ind6 = ind0[ind3][np.nanargmax(yval[ind3])]
-            else:
+                    xpks = [xval[ind6]]
+                    ypks = [yval[ind6]]
+            else: # monotonic
                 ind6 = ind0[ind3][np.nanargmax(yval[ind3])]
-            xpek = xval[ind6]
-            ypek = np.nanmax(yval[ind3])
-            fp.write('{:6d} {:13.6e} {:13.6e}\n'.format(sid,xpek,ypek))
+                xpks = [xval[ind6]]
+                ypks = [yval[ind6]]
+            for xpek,ypek in zip(xpks,ypks):
+                fp.write('{:6d} {:15.8e} {:13.6e}\n'.format(sid,xpek,ypek))
             if opts.debug:
                 ax1.hlines(opts.ythr,xval[si],xval[ei],zorder=10)
-                ax1.plot(xval[ind6],yval[ind6],'ro',zorder=10)
+                if opts.subpeak and len(xpks) > 1:
+                    for xpek,ypek in zip(xpks,ypks):
+                        ax1.plot(xpek,ypek,'ro',zorder=10)
+                else:
+                    ax1.plot(xval[ind6],yval[ind6],'ro',zorder=10)
         if opts.debug:
             ax1.plot(xval,yval,'b-',zorder=1)
             ax1.set_ylabel('Signal value (dB)')
@@ -197,6 +239,7 @@ with open(opts.outnam,'w') as fp:
             fig.autofmt_xdate()
             plt.draw()
             plt.savefig(pdf,format='pdf')
-        #break
+        if opts.enum is not None and sid >= opts.enum:
+            break
 if opts.debug:
     pdf.close()
