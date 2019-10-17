804242fbd694ea04b6ba8fe22eab466162ec9e11
Naohiro MANAGO <manago.naohiro@chiba-u.jp>
2019-10-17 10:45:49 +0900

    NM modified to specify range.

--- draw_transdate_shapefile.py
+++ draw_transdate_shapefile.py
@@ -1,4 +1,5 @@
 #from shapely.geometry.polygon import Polygon
+import sys
 from datetime import datetime
 import numpy as np
 import shapefile
@@ -16,6 +17,8 @@ from optparse import OptionParser,IndentedHelpFormatter
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
 parser.add_option('-s','--tmin',default=None,help='Min date in the format YYYYMMDD (%default)')
 parser.add_option('-e','--tmax',default=None,help='Max date in the format YYYYMMDD (%default)')
+parser.add_option('-p','--pmin',default=None,type='float',help='Min signal in dB (%default)')
+parser.add_option('-P','--pmax',default=None,type='float',help='Max signal in dB (%default)')
 parser.add_option('-t','--title',default=None,help='Figure title (%default)')
 (opts,args) = parser.parse_args()
 
@@ -50,17 +53,34 @@ xg,yg = np.meshgrid(xp,yp)
 
 tmin = 1.0e10
 tmax = -1.0e10
+pmin = 1.0e10
+pmax = -1.0e10
 for rec in records:
     t = rec.attributes['trans_date']+utc_offset
-    if t > 1000 and t < tmin:
-        tmin = t
-    if t > tmax:
-        tmax = t
+    p = rec.attributes['peak_value']
+    if t > 1000.0:
+        if t < tmin:
+            tmin = t
+        if t > tmax:
+            tmax = t
+        if p < pmin:
+            pmin = p
+        if p > pmax:
+            pmax = p
+sys.stderr.write('tmin: {}\n'.format(num2date(tmin).strftime('%Y%m%d')))
+sys.stderr.write('tmax: {}\n'.format(num2date(tmax).strftime('%Y%m%d')))
+sys.stderr.write('pmin: {}\n'.format(pmin))
+sys.stderr.write('pmax: {}\n'.format(pmax))
 if opts.tmin is not None:
     tmin = date2num(datetime.strptime(opts.tmin,'%Y%m%d'))
 if opts.tmax is not None:
     tmax = date2num(datetime.strptime(opts.tmax,'%Y%m%d'))
+if opts.pmin is not None:
+    pmin = opts.pmin
+if opts.pmax is not None:
+    pmax = opts.pmax
 tdif = tmax-tmin
+pdif = pmax-pmin
 
 values = []
 labels = []
@@ -94,16 +114,35 @@ for y in range(num2date(tmin).year,num2date(tmax).year+1):
                 ticks.append(date2num(d))
 
 plt.interactive(True)
-fig = plt.figure(1,facecolor='w',figsize=(7,6))
+fig = plt.figure(1,facecolor='w',figsize=(12,6))
 fig.clear()
-plt.subplots_adjust(top=0.93,bottom=0.03,left=0.10,right=0.80)
-ax1 = plt.subplot(111,projection=prj)
+plt.subplots_adjust(top=0.93,bottom=0.03,left=0.02,right=0.95,wspace=0.4)
+
+ax1 = plt.subplot(121,projection=prj)
+ax2 = plt.subplot(122,projection=prj)
 for shp,rec in zip(shapes,records):
     t = rec.attributes['trans_date']+utc_offset
+    p = rec.attributes['peak_value']
     if t > 1000.0:
         ax1.add_geometries(shp,prj,edgecolor='none',facecolor=cm.jet((t-tmin)/tdif))
-im = ax1.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=tmin,vmax=tmax,cmap=cm.jet)
+        ax2.add_geometries(shp,prj,edgecolor='none',facecolor=cm.jet((p-pmin)/pdif))
+im1 = ax1.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=tmin,vmax=tmax,cmap=cm.jet)
+ax12 = plt.colorbar(im1,ax=ax1).ax
+ax12.yaxis.set_major_locator(plt.FixedLocator(values))
+ax12.yaxis.set_major_formatter(plt.FixedFormatter(labels))
+ax12.yaxis.set_minor_locator(plt.FixedLocator(ticks))
+ax12.set_ylabel('Transplanting date')
+ax12.yaxis.set_label_coords(5.5,0.5)
 ax1.add_geometries(block_shp,prj,edgecolor='k',facecolor='none')
+im2 = ax2.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=pmin,vmax=pmax,cmap=cm.jet)
+ax22 = plt.colorbar(im2,ax=ax2).ax
+ax22.minorticks_on()
+#ax22.yaxis.set_major_locator(plt.FixedLocator(values))
+#ax22.yaxis.set_major_formatter(plt.FixedFormatter(labels))
+#ax22.yaxis.set_minor_locator(plt.FixedLocator(ticks))
+ax22.set_ylabel('Signal (dB)')
+ax22.yaxis.set_label_coords(3.5,0.5)
+ax2.add_geometries(block_shp,prj,edgecolor='k',facecolor='none')
 for bs,br in zip(block_shp,block_rec):
     for n in range(len(bs)):
         #xc = bs[0].boundary.centroid.x
@@ -125,13 +164,11 @@ for bs,br in zip(block_shp,block_rec):
             t = br.attributes['Blok']
             ax1.text(xt,yt,t,transform=prj,ha='center',va='center')
         #break
-ax2 = plt.colorbar(im).ax
-ax2.yaxis.set_major_locator(plt.FixedLocator(values))
-ax2.yaxis.set_major_formatter(plt.FixedFormatter(labels))
-ax2.yaxis.set_minor_locator(plt.FixedLocator(ticks))
 ax1.set_xlim(xmin,xmax)
 ax1.set_ylim(ymin,ymax)
+ax2.set_xlim(xmin,xmax)
+ax2.set_ylim(ymin,ymax)
 if opts.title is not None:
-    ax1.set_title(opts.title)
+    plt.suptitle(opts.title)
 plt.draw()
 plt.savefig('transplanting_date.pdf')
