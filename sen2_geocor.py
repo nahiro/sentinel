ca355f0e089e5b6d6aa7728b8d02a1898f093f60
Naohiro MANAGO <manago.naohiro@gmail.com>
2019-10-24 12:58:58 +0900

    NM modified to accept options.

--- sen2_geocor.py
+++ sen2_geocor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import sys
 import re
@@ -8,6 +9,27 @@ import xml.etree.ElementTree as ET
 import numpy as np
 from scipy.interpolate import interp2d
 from scipy.optimize import leastsq
+from optparse import OptionParser,IndentedHelpFormatter
+
+# Default values
+WIDTH = 100
+HEIGHT = 100
+FEPS = 0.1
+RTHR = 0.3
+
+# Read options
+parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
+parser.set_usage('Usage: %prog reference_geotiff_file target_geotiff_file [options]')
+parser.add_option('-W','--trg_subset_width',default=WIDTH,type='int',help='Subset width in target pixel (%default)')
+parser.add_option('-H','--trg_subset_height',default=HEIGHT,type='int',help='Subset height in target pixel (%default)')
+parser.add_option('-r','--rthr',default=RTHR,type='float',help='Threshold of correlation coefficient (%default)')
+parser.add_option('-E','--feps',default=FEPS,type='float',help='Step length for curve_fit (%default)')
+(opts,args) = parser.parse_args()
+if len(args) < 1:
+    parser.print_help()
+    sys.exit(0)
+ref_fnam = args[0]
+trg_fnam = args[1]
 
 def residuals(p,refx,refy,refz,trgx,trgy,trgz):
     pmax = np.abs(p).max()
@@ -17,10 +39,6 @@ def residuals(p,refx,refy,refz,trgx,trgy,trgz):
     intz = f(refx,refy)[::-1]
     r = np.corrcoef(intz.flatten(),refz.flatten())[0,1]
     return np.full(10,1.0-r)
-
-datdir = '/home/naohiro/Work/Sentinel-2/191022'
-
-ref_fnam = os.path.join(datdir,'wv2_180629_mul.tif')
 
 tif_tags = {}
 with tifffile.TiffFile(ref_fnam) as tif:
@@ -58,11 +76,12 @@ ref_xp_min = ref_xp0.min()
 ref_xp_max = ref_xp0.max()
 ref_yp_min = ref_yp0.min()
 ref_yp_max = ref_yp0.max()
+ref_xp_stp = ref_xp0[1]-ref_xp0[0]
 ref_yp_stp = ref_yp0[1]-ref_yp0[0]
+if ref_xp_stp <= 0.0:
+    raise ValueError('Error, ref_xp_stp={}'.format(ref_xp_stp))
 if ref_yp_stp >= 0.0:
     raise ValueError('Error, ref_yp_stp={}'.format(ref_yp_stp))
-
-trg_fnam = os.path.join(datdir,'20190525.tif')
 
 tif_tags = {}
 with tifffile.TiffFile(trg_fnam) as tif:
@@ -98,22 +117,24 @@ trg_xp_min = trg_xp0.min()
 trg_xp_max = trg_xp0.max()
 trg_yp_min = trg_yp0.min()
 trg_yp_max = trg_yp0.max()
+trg_xp_stp = trg_xp0[1]-trg_xp0[0]
 trg_yp_stp = trg_yp0[1]-trg_yp0[0]
+if trg_xp_stp <= 0.0:
+    raise ValueError('Error, trg_xp_stp={}'.format(trg_xp_stp))
 if trg_yp_stp >= 0.0:
     raise ValueError('Error, trg_yp_stp={}'.format(trg_yp_stp))
 
 ref_height,ref_width = ref_data.shape
 trg_height,trg_width = trg_data.shape
 
-template_width = 100 # target pixel
-template_height = 100 # target pixel
-template_half_width = template_width//2
-template_half_height = template_height//2
+template_half_width = opts.trg_subset_width//2
+template_half_height = opts.trg_subset_height//2
 shift_width = 3 # target pixel
 shift_height = 3 # target pixel
 margin_width = 5 # target_pixel
 margin_height = 5 # target_pixel
-for trg_indyc in np.arange(0,trg_height,template_half_height):
+#for trg_indyc in np.arange(0,trg_height,template_half_height):
+for trg_indyc in [450]:
     trg_indy1 = trg_indyc-template_half_height-margin_height
     trg_indy2 = trg_indyc+template_half_height+margin_height+1
     if trg_indy1 < 0:
@@ -134,7 +155,8 @@ for trg_indyc in np.arange(0,trg_height,template_half_height):
     if ref_indy2.size < 1:
         continue
     ref_indy2 = ref_indy2[-1]+1
-    for trg_indxc in np.arange(0,trg_width,template_half_width):
+    #for trg_indxc in np.arange(0,trg_width,template_half_width):
+    for trg_indxc in [1250]:
         trg_indx1 = trg_indxc-template_half_width-margin_width
         trg_indx2 = trg_indxc+template_half_width+margin_width+1
         if trg_indx1 < 0:
@@ -166,9 +188,13 @@ for trg_indyc in np.arange(0,trg_height,template_half_height):
         if ref_subset_data.min() < 1:
             continue
         p1 = np.array([0.0,0.0])
-        result = leastsq(residuals,p1,args=(ref_subset_xp0,ref_subset_yp0,ref_subset_data,trg_subset_xp0,trg_subset_yp0,trg_subset_data),epsfcn=1.0e-1,full_output=True)
+        result = leastsq(residuals,p1,args=(ref_subset_xp0,ref_subset_yp0,ref_subset_data,trg_subset_xp0,trg_subset_yp0,trg_subset_data),epsfcn=opts.feps,full_output=True)
         p2 = result[0]
         r = 1.0-result[2]['fvec'][0]
-        sys.stdout.write('{:6d} {:6d} {:8.2f} {:8.2f} {:6.2f} {:6.2f} {:8.3f}\n'.format(trg_indxc,trg_indyc,trg_xp0[trg_indxc]+p2[0],trg_yp0[trg_indyc]+p2[1],p2[0],p2[1],r))
-        #break
-    #break
+        #f = interp2d(trg_subset_xp0+p2[0],trg_subset_yp0+p2[1],trg_subset_data,kind='linear')
+        #trg_interp_data = f(ref_subset_xp0,ref_subset_yp0)[::-1]
+        #r = np.corrcoef(trg_interp_data.flatten(),ref_subset_data.flatten())[0,1]
+        if r > opts.rthr:
+            sys.stdout.write('{:6d} {:6d} {:8.2f} {:8.2f} {:6.2f} {:6.2f} {:8.3f}\n'.format(trg_indxc,trg_indyc,trg_xp0[trg_indxc]+p2[0],trg_yp0[trg_indyc]+p2[1],p2[0],p2[1],r))
+        break
+    break
