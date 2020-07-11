bf5b3d62d3a562773ac324744e7ce9d7875981b3
Naohiro MANAGO <manago.naohiro@gmail.com>
2020-07-11 16:23:15 +0900

    NM increased number of fields to be averaged.

--- find_nearest_field.py
+++ find_nearest_field.py
@@ -2,15 +2,33 @@
 import sys
 import numpy as np
 
+n_nearest = 36
+
 sid,xc,yc,ndat,leng,area = np.loadtxt('get_center.dat',unpack=True)
 sid = (sid+0.1).astype(np.int64)
 ndat = (ndat+0.1).astype(np.int64)
 
-for i in range(sid.size):
-    l2 = np.square(xc-xc[i])+np.square(yc-yc[i])
+sid_0 = []
+for nn in range(1,n_nearest+1):
+    exec('sid_{} = []'.format(nn))
+    exec('leng_{} = []'.format(nn))
+for n in range(sid.size):
+    l2 = np.square(xc-xc[n])+np.square(yc-yc[n])
     indx = np.argsort(l2)
-    sys.stdout.write('{:6d}'.format(i))
-    for j in indx[1:13]:
-        sys.stdout.write(' {:6d} {:4.1f}'.format(sid[j],np.sqrt(l2[j])))
-    sys.stdout.write('\n')
-        
+    if sid[indx[0]] != n:
+        raise ValueError('Error, indx[0]={}, n={}'.format(indx[0],n))
+    sid_0.append(n)
+    for nn in range(1,n_nearest+1):
+        exec('sid_{}.append(sid[indx[{}]])'.format(nn,nn))
+        exec('leng_{}.append(np.sqrt(l2[indx[{}]]))'.format(nn,nn))
+    #break
+sid_0 = np.array(sid_0)
+for nn in range(1,n_nearest+1):
+    exec('sid_{} = np.array(sid_{})'.format(nn,nn))
+    exec('leng_{} = np.array(leng_{})'.format(nn,nn))
+command = 'np.savez("find_nearest.npz",'
+command += 'sid_0=sid_0,'
+for nn in range(1,n_nearest+1):
+    command += 'sid_{}=sid_{},leng_{}=leng_{},'.format(nn,nn,nn,nn)
+command += ')'
+exec(command)
