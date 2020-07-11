051652c252dd90f599766422930ac2c84e56ba36
Naohiro MANAGO <manago.naohiro@gmail.com>
2020-07-11 15:16:52 +0900

    NM increased number of field to be averaged.

--- find_nearest.py
+++ find_nearest.py
@@ -10,7 +10,7 @@ for i in range(sid.size):
     l2 = np.square(xc-xc[i])+np.square(yc-yc[i])
     indx = np.argsort(l2)
     sys.stdout.write('{:6d}'.format(i))
-    for j in indx[1:11]:
+    for j in indx[1:13]:
         sys.stdout.write(' {:6d} {:4.1f}'.format(sid[j],np.sqrt(l2[j])))
     sys.stdout.write('\n')
         
