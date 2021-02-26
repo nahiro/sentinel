aa48bb36047d923b37c0245155c9f763dfa9241f
Naohiro MANAGO <manago.naohiro@gmail.com>
2021-02-26 15:28:08 +0900

    NM modified files.

--- sentinel2_atcor.py
+++ sentinel2_atcor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import warnings
 import gdal
 from datetime import datetime
 import numpy as np
@@ -13,14 +14,20 @@ from matplotlib.backends.backend_pdf import PdfPages
 from optparse import OptionParser,IndentedHelpFormatter
 
 # Default values
+BAND = '4'
+TMIN = '20190401'
+TMAX = '20190615'
+VTHR = 0.02
 DATDIR = '..'
 INDS_FNAM = 'nearest_inds_500.npy'
 FIG_FNAM = 'sentinel2_atcor.pdf'
 
 # Read options
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
+parser.add_option('-b','--band',default=BAND,help='Target band (%default)')
 parser.add_option('-s','--tmin',default=TMIN,help='Min date of transplanting in the format YYYYMMDD (%default)')
 parser.add_option('-e','--tmax',default=TMAX,help='Max date of transplanting in the format YYYYMMDD (%default)')
+parser.add_option('-v','--vthr',default=VTHR,type='float',help='Threshold to remove outliers (%default)')
 parser.add_option('-D','--datdir',default=DATDIR,help='Input data directory (%default)')
 parser.add_option('--inds_fnam',default=INDS_FNAM,help='Index file name (%default)')
 parser.add_option('-F','--fig_fnam',default=FIG_FNAM,help='Output figure name for debug (%default)')
@@ -30,9 +37,9 @@ parser.add_option('--debug',default=False,action='store_true',help='Debug mode (
 nmin = date2num(datetime.strptime(opts.tmin,'%Y%m%d'))
 nmax = date2num(datetime.strptime(opts.tmax,'%Y%m%d'))
 ntim = np.load(os.path.join(opts.datdir,'ntim.npy'))
-b4 = np.load(os.path.join(opts.datdir,'band4.npy'))
-b4_mean = np.load(os.path.join(opts.datdir,'b4_mean.npy'))
-b4_std = np.load(os.path.join(opts.datdir,'b4_std.npy'))
+data_x_all = np.load(os.path.join(opts.datdir,'band{}.npy'.format(opts.band)))
+data_y_all = np.load(os.path.join(opts.datdir,'b{}_mean.npy'.format(opts.band)))
+data_z_all = np.load(os.path.join(opts.datdir,'b{}_std.npy'.format(opts.band)))
 nearest_inds = np.load(opts.inds_fnam)
 nobject = len(nearest_inds)
 
@@ -41,6 +48,8 @@ if opts.debug:
     plt.subplots_adjust(top=0.85,bottom=0.20,left=0.15,right=0.90)
     pdf = PdfPages(opts.fig_fnam)
     xfit = np.arange(0.0,0.51,0.01)
+else:
+    warnings.simplefilter('ignore')
 
 for indt in range(ntim.size):
     if ntim[indt] < nmin-1.0e-3 or ntim[indt] > nmax+1.0e-3:
@@ -55,26 +64,26 @@ for indt in range(ntim.size):
             print(i)
         object_id = i+1
         indx = nearest_inds[i]
-        b4_org = b4[indt,indx]
-        b4_org_mean = b4_mean[indx]
-        b4_org_std = b4_std[indx]
-        cnd = ~np.isnan(b4_org)
-        xcnd = b4_org[cnd]
-        ycnd = b4_org_mean[cnd]
+        data_x = data_x_all[indt,indx]
+        data_y = data_x_all[indx]
+        data_z = data_x_all[indx]
+        cnd = ~np.isnan(data_x)
+        xcnd = data_x[cnd]
+        ycnd = data_y[cnd]
         result = np.polyfit(xcnd,ycnd,1)
-        b4_calc = xcnd*result[0]+result[1]
-        cnd2 = np.abs(b4_calc-ycnd) < 0.02
+        calc_y = xcnd*result[0]+result[1]
+        cnd2 = np.abs(calc_y-ycnd) < opts.vthr
         if cnd2.sum() != cnd2.size:
             flag = True
             xcnd2 = xcnd[cnd2]
             ycnd2 = ycnd[cnd2]
-            zcnd2 = b4_org_std[cnd][cnd2]
+            zcnd2 = data_z[cnd][cnd2]
             result = np.polyfit(xcnd2,ycnd2,1)
-            b4_calc = xcnd2*result[0]+result[1]
-            rms_value = np.sqrt(np.square(b4_calc-ycnd2).sum()/b4_calc.size)
+            calc_y = xcnd2*result[0]+result[1]
+            rms_value = np.sqrt(np.square(calc_y-ycnd2).sum()/calc_y.size)
         else:
             flag = False
-            rms_value = np.sqrt(np.square(b4_calc-ycnd).sum()/b4_calc.size)
+            rms_value = np.sqrt(np.square(calc_y-ycnd).sum()/calc_y.size)
         factor.append(result[0])
         offset.append(result[1])
         rmse.append(rms_value)
@@ -83,7 +92,7 @@ for indt in range(ntim.size):
             ax1 = plt.subplot(111,aspect='equal')
             ax1.minorticks_on()
             ax1.grid(True)
-            line = 'number: {}\n'.format(b4_calc.size)
+            line = 'number: {}\n'.format(calc_y.size)
             line += 'factor: {:5.4f}\n'.format(result[0])
             line += 'offset: {:5.4f}\n'.format(result[1])
             line += 'rmse: {:5.4f}'.format(rms_value)
@@ -94,15 +103,15 @@ for indt in range(ntim.size):
                 ax1.scatter(xcnd,ycnd,c='k',marker='.')
                 im = ax1.scatter(xcnd2,ycnd2,c=zcnd2,marker='.',cmap=cm.jet,vmin=0.01,vmax=0.035)
             else:
-                im = ax1.scatter(b4_org,b4_org_mean,c=b4_org_std,marker='.',cmap=cm.jet,vmin=0.01,vmax=0.035)
+                im = ax1.scatter(data_x,data_y,c=data_z,marker='.',cmap=cm.jet,vmin=0.01,vmax=0.035)
             ax1.plot(xfit,np.polyval(result,xfit),'k:')
             ax2 = plt.colorbar(im,ticks=np.arange(0.0,0.051,0.01)).ax
             ax2.minorticks_on()
-            ax2.set_ylabel('Band4 std')
+            ax2.set_ylabel('Band{} std'.format(opts.band))
             ax1.set_xlim(0.0,0.5)
             ax1.set_ylim(0.0,0.5)
-            ax1.set_xlabel('Band4')
-            ax1.set_ylabel('Band4 mean')
+            ax1.set_xlabel('Band{}'.format(opts.band))
+            ax1.set_ylabel('Band{} mean'.format(opts.band))
             ax1.xaxis.set_tick_params(pad=7)
             ax1.xaxis.set_label_coords(0.5,-0.14)
             ax1.yaxis.set_label_coords(-0.15,0.5)
@@ -117,6 +126,6 @@ for indt in range(ntim.size):
     factor = np.array(factor)
     offset = np.array(offset)
     rmse = np.array(rmse)
-    np.save('b4_factor_{}.npy'.format(dstr),factor)
-    np.save('b4_offset_{}.npy'.format(dstr),offset)
-    np.save('b4_rmse_{}.npy'.format(dstr),rmse)
+    np.save('atcor_factor_{}.npy'.format(dstr),factor)
+    np.save('atcor_offset_{}.npy'.format(dstr),offset)
+    np.save('atcor_rmse_{}.npy'.format(dstr),rmse)
