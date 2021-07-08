e26eb73da42235493e30778873908afc1dd4fbdd
Naohiro MANAGO <manago.naohiro@gmail.com>
2021-07-08 20:15:45 +0900

    NM modified files.

--- sentinel_wget.py
+++ sentinel_wget.py
@@ -4,23 +4,26 @@ import psutil
 import sys
 import re
 import time
-import numpy as np
 import xml.etree.ElementTree as ET
 from collections import OrderedDict
 from datetime import datetime
-from glob import glob
-from subprocess import Popen,check_output,PIPE,STDOUT
+from subprocess import check_output,PIPE,STDOUT
 from optparse import OptionParser,IndentedHelpFormatter
 
 # Defaults
-SERVER = 'https://scihub.copernicus.eu/dhus'
+USER = os.environ.get('DHUS_USER')
+PASSWORD = os.environ.get('DHUS_PASSWORD')
+URL = os.environ.get('DHUS_URL')
+if URL is None:
+    URL = 'https://scihub.copernicus.eu/dhus'
+ONLINE_CHECK_TIME = 300
+MAX_RETRY = 10
 
 # Read options
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
-parser.add_option('--server',default=SERVER,help='Server URL (%default)')
-parser.add_option('-u','--user',default=None,help='Username (or environment variable DHUS_USER is set)')
-parser.add_option('-p','--password',default=None,help='Password (or environment variable DHUS_PASSWORD is set)')
-parser.add_option('-U','--url',default=None,help='API URL (or environment variable DHUS_URL is set) (%default)')
+parser.add_option('-u','--user',default=USER,help='Username (or environment variable DHUS_USER is set)')
+parser.add_option('-p','--password',default=PASSWORD,help='Password (or environment variable DHUS_PASSWORD is set)')
+parser.add_option('-U','--url',default=URL,help='API URL (or environment variable DHUS_URL is set)')
 parser.add_option('-s','--start',default=None,help='Start date of the query in the format YYYYMMDD.')
 parser.add_option('-e','--end',default=None,help='End date of the query in the format YYYYMMDD.')
 parser.add_option('-g','--geometry',default=None,help='Search area geometry as GeoJSON file.')
@@ -35,6 +38,8 @@ parser.add_option('-l','--limit',default=None,type='int',help='Maximum number of
 parser.add_option('-L','--log',default=None,help='Set the log file name.')
 parser.add_option('-P','--path',default=None,help='Set the path where the files will be saved.')
 parser.add_option('-q','--query',default=None,help='Extra search keywords you want to use in the query. Separate keywords with comma. Example: \'producttype=GRD,polarisationmode=HH\'.')
+parser.add_option('-O','--online_check_time',default=ONLINE_CHECK_TIME,type='int',help='Wait time to check online data in sec (%default)')
+parser.add_option('-M','--max_retry',default=MAX_RETRY,type='int',help='Maximum number of retries to download data (%default)')
 parser.add_option('-d','--download',default=False,action='store_true',help='Download all results of the query. (%default)')
 parser.add_option('-Y','--sort_year',default=False,action='store_true',help='Sort files by year. (%default)')
 parser.add_option('-C','--checksum',default=False,action='store_true',help='Verify the downloaded files\' integrity by checking its MD5 checksum. (%default)')
@@ -43,6 +48,7 @@ parser.add_option('-v','--version',default=False,action='store_true',help='Show 
 parser.add_option('-Q','--quiet',default=False,action='store_true',help='Quiet mode (%default)')
 (opts,args) = parser.parse_args()
 
+# Query products
 command = 'sentinelsat'
 if opts.user is not None:
     command += ' --user {}'.format(opts.user)
@@ -84,13 +90,11 @@ if opts.version:
     command += ' --version'
 sys.stderr.write(command+'\n')
 sys.stderr.flush()
-#out = check_output(command,shell=True,stderr=PIPE).decode()
 out = check_output(command,shell=True,stderr=STDOUT).decode()
 sys.stderr.write(out+'\n')
 sys.stderr.flush()
 if opts.version:
     sys.exit()
-
 uuids = []
 for line in out.splitlines():
     # Product bb7a7783-f91b-4a35-907d-a6ddb807da73 - Date: 2019-07-09T02:55:59.024Z, Instrument: MSI, Mode: , Satellite: Sentinel-2, Size: 1.09 GB
@@ -99,6 +103,7 @@ for line in out.splitlines():
         continue
     uuids.append(m.group(1))
 
+# Get details of products
 names = []
 sizes = []
 stats = []
@@ -115,6 +120,8 @@ for i,uuid in enumerate(uuids):
         if postfix.lower() == 'properties':
             child = value
             break
+    if child is None:
+        raise ValueError('Error, faild in finding properies >>> {}'.format(uuid))
     tags = OrderedDict()
     for value in child:
         prefix,has_namespace,postfix = value.tag.rpartition('}')
@@ -133,8 +140,20 @@ for i,uuid in enumerate(uuids):
     stats.append(stat)
     sys.stderr.write('{:4d} {:40s} {:70s} {:10d} {:7s}\n'.format(i+1,uuid,name,size,'Online' if stat else 'Offline'))
     sys.stderr.flush()
+    # If offline, order products from the historical archives
+    if not stat:
+        command = 'wget'
+        if opts.user is not None:
+            command += ' --user {}'.format(opts.user)
+        if opts.password is not None:
+            command += ' --password {}'.format(opts.password)
+        command += ' --content-disposition'
+        command += ' --continue'
+        if opts.quiet:
+            command += ' --quiet'
+        command += ' --output-document '+gnam
+        command += ' "'+opts.server+'/odata/v1/Products(\'{}\')/\$value"'.format(uuid)
 
-"""
 if opts.download:
     path = '.' if opts.path is None else opts.path
     for i in range(len(uuids)):
@@ -153,44 +172,65 @@ if opts.download:
             dnam = path
         fnam = os.path.join(dnam,names[i]+'.zip')
         gnam = os.path.join(fnam+'.incomplete')
-        # Skip if gnam does not exist and fnam with expected size exists
-        if os.path.exists(fnam) and not os.path.exists(gnam):
+        # Rename if gnam with expected size exists
+        if os.path.exists(gnam):
+            fsiz = os.path.getsize(gnam)
+            if fsiz == sizes[i]:
+                os.rename(gnam,fnam)
+        # Skip if fnam with expected size exists
+        if os.path.exists(fnam):
             fsiz = os.path.getsize(fnam)
             if fsiz == sizes[i]:
                 sys.stderr.write('###### Successfully downloaded >>> {}\n'.format(fnam))
                 sys.stderr.flush()
-                continue
-        gnam = os.path.join(fnam+'.request')
-        if opts.url is not None:
-            api = SentinelAPI(opts.user,opts.password,opts.url)
-        else:
-            api = SentinelAPI(opts.user,opts.password)
-        while True:
-            out = api.get_product_odata(uuids[i])
-            stat = out['Online']
-            if stat: # Online
                 if os.path.exists(gnam):
                     os.remove(gnam)
+                continue
+        # Wait online
+        while True:
+            command = 'wget'
+            command += ' --no-check-certificate'
+            command += ' --output-document -'
+            command += ' "'+opts.server+'/odata/v1/Products(\'{}\')"'.format(uuids[i])
+            out = check_output(command,shell=True,stderr=PIPE).decode()
+            root = ET.fromstring(out)
+            child = None
+            for value in root:
+                prefix,has_namespace,postfix = value.tag.rpartition('}')
+                if postfix.lower() == 'properties':
+                    child = value
+                    break
+            if child is None:
+                raise ValueError('Error, faild in finding properies >>> {}'.format(uuids[i]))
+            tags = OrderedDict()
+            for value in child:
+                prefix,has_namespace,postfix = value.tag.rpartition('}')
+                if len(value) > 0:
+                    tags[postfix] = OrderedDict()
+                    for value_2 in value:
+                        prefix_2,has_namespace_2,postfix_2 = value_2.tag.rpartition('}')
+                        tags[postfix][postfix_2] = value_2.text
+                else:
+                    tags[postfix] = value.text
+            stat = eval(tags['Online'].capitalize())
+            if stat: # Online
                 break
             sys.stderr.write('Offline. Wait for {} sec >>> {}\n'.format(opts.online_check_time,fnam))
             sys.stderr.flush()
             time.sleep(opts.online_check_time)
             continue
-        api.session.close() # has any effect?
         # Download data
-        gnam = os.path.join(fnam+'.incomplete')
-        command = 'sentinel_wget.py'
-        command += ' --download'
-        command += ' --uuid {}'.format(uuids[i])
+        command = 'wget'
         if opts.user is not None:
             command += ' --user {}'.format(opts.user)
         if opts.password is not None:
             command += ' --password {}'.format(opts.password)
-        if opts.url is not None:
-            command += ' --url {}'.format(opts.url)
-        command += ' --path {}'.format(dnam)
-        if not opts.checksum:
-            command += ' --no-checksum'
+        command += ' --content-disposition'
+        command += ' --continue'
+        if opts.quiet:
+            command += ' --quiet'
+        command += ' --output-document '+gnam
+        command += ' "'+opts.server+'/odata/v1/Products(\'{}\')/\$value"'.format(uuids[i])
         for ntry in range(opts.max_retry): # loop to download 1 file
             # Exit if gnam does not exist and fnam with expected size exists
             if os.path.exists(fnam):
@@ -212,44 +252,3 @@ if opts.download:
                         break
                     else:
                         os.rename(fnam,gnam)
-            # Start a process
-            p = None
-            process_id = None
-            start_time = time.time()
-            try:
-                if opts.quiet:
-                    if os.name.lower() != 'posix':
-                        p = Popen(command,stdout=PIPE,stderr=PIPE,bufsize=0,universal_newlines=True,encoding='utf-8',shell=True)
-                    else:
-                        p = Popen(command,stdout=PIPE,stderr=PIPE,bufsize=0,universal_newlines=True,encoding='utf-8',shell=True,preexec_fn=os.setsid)
-                else:
-                    if os.name.lower() != 'posix':
-                        p = Popen(command,universal_newlines=True,encoding='utf-8',shell=True)
-                    else:
-                        p = Popen(command,universal_newlines=True,encoding='utf-8',shell=True,preexec_fn=os.setsid)
-                process_id = p.pid
-            except Exception:
-                sys.stderr.write('Failed to run the command. Wait for {} sec\n'.format(opts.wait_time))
-                sys.stderr.flush()
-                time.sleep(opts.wait_time)
-                continue
-            while True: # loop to terminate the process
-                # Exit if fnam exists or gnam exists and its size does not change for opts.timeout seconds
-                if p.poll() is not None: # the process has terminated
-                    break
-                elif os.path.exists(fnam):
-                    break
-                elif os.path.exists(gnam):
-                    diff = time.time()-max(start_time,os.path.getmtime(gnam))
-                    if diff > opts.timeout:
-                        break
-                time.sleep(opts.download_check_time)
-            if p.poll() is None: # the process hasn't terminated yet.
-                sys.stderr.write('\nTimeout\n')
-                sys.stderr.flush()
-            kill_process()
-            if opts.quiet:
-                out,err = p.communicate()
-                sys.stderr.write('\n')
-                sys.stderr.flush()
-"""
