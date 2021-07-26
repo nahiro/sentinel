90badf126b5882e6aa80df78a31c7fd62db043ab
Naohiro MANAGO <manago.naohiro@gmail.com>
2021-07-26 15:35:27 +0900

    NM modified files.

--- read_shapefile.py
+++ read_shapefile.py
@@ -9,6 +9,7 @@ LENG = 20
 # Read options
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
 parser.add_option('-l','--leng',default=LENG,type='int',help='Item length (%default)')
+parser.add_option('-n','--field_name',default=False,action='store_true',help='Output field name only (%default)')
 (opts,args) = parser.parse_args()
 if len(args) < 1:
     parser.print_help()
@@ -22,6 +23,8 @@ sys.stdout.write('{:>6s}'.format('Index'))
 for rec in r.fields[-len(r.record()):]:
     sys.stdout.write(item_format.format(str(rec[0]).strip()))
 sys.stdout.write('\n')
+if opts.field_name:
+    sys.exit()
 for i,shaperec in enumerate(r.iterShapeRecords()):
     sys.stdout.write('{:6d}'.format(i))
     for rec in shaperec.record:
