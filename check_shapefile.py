07ffc3e00b4bbb54e962f88855f9fec094de13a8
Naohiro MANAGO <manago.naohiro@gmail.com>
2021-08-26 15:53:45 +0900

    NM modified files.

--- check_shapefile.py
+++ check_shapefile.py
@@ -6,9 +6,13 @@ import shapefile
 from shapely.geometry import Point,Polygon
 from optparse import OptionParser,IndentedHelpFormatter
 
+# Default values
+RMAX = 1.0
+
 # Read options
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
 parser.set_usage('Usage: %prog shapefile_for_checking [options]')
+parser.add_option('-r','--rmax',default=RMAX,type='float',help='Maximum distance in meter to be detected (%default)')
 (opts,args) = parser.parse_args()
 if len(args) < 1:
     parser.print_help()
@@ -21,13 +25,14 @@ xs = []
 ys = []
 ps = []
 r = shapefile.Reader(shpnam)
+#r = shapefile.Reader(shpnam,encoding='cp1252') # cp1252 for Windows
 for i,shaperec in enumerate(r.iterShapeRecords()):
     rec = shaperec.record
     shp = shaperec.shape
     # Check if the polygon is valid
     p1 = Polygon(shp.points)
     if not p1.is_valid:
-        sys.stderr.write('Error, FID {} is not valid\n'.format(i))
+        sys.stderr.write('Error, FID {:6d} is not valid\n'.format(i))
     # Check if the barycenter and the center of bbox is the same -> not needed
     pp = np.array(shp.points)
     xc = pp[:,0].mean()
@@ -45,13 +50,13 @@ xs = np.array(xs)
 ys = np.array(ys)
 
 n = np.arange(xs.size)
-# Check is the polygon is unique
+# Check if the polygon is unique
 for i in range(xs.size):
-    l2 = np.square(xs-xs[i])+np.square(ys-ys[i])
-    indx = np.where((l2 < 1.0e-4) & (n != i))[0]
+    r = np.sqrt(np.square(xs-xs[i])+np.square(ys-ys[i]))
+    indx = n[(~np.isnan(r)) & (r < opts.rmax) & (n != i)]
     if len(indx) != 0:
-        sys.stderr.write('Warning, i={:6d}'.format(i))
+        sys.stderr.write('Warning, FID={:6d}'.format(i))
         for j in indx:
-            sys.stderr.write(', l2[{:6d}]={}, all_equal={}'.format(j,l2[j],np.all(ps[i]==ps[j])))
+            sys.stderr.write(', r[{:6d}]={:13.6e}, all_equal={}'.format(j,r[j],np.all(ps[i]==ps[j])))
             xs[j] = np.nan
         sys.stderr.write('\n')
