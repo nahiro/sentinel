4d12c1bf0a1317b34bdbc7bce495cd4e7f891ed2
Naohiro MANAGO <manago.naohiro@gmail.com>
2021-09-17 10:07:51 +0900

    NM modified files.

--- field_block.py
+++ field_block.py
@@ -3,27 +3,44 @@ import sys
 import numpy as np
 import shapefile
 from shapely.geometry import Point,Polygon
+from optparse import OptionParser,IndentedHelpFormatter
 
-shp_fnam1 = '/home/naohiro/Work/SATREPS/Shapefile/cihea_testsite_200819/cihea_testsite_200819.shp'
-shp_fnam2 = '/home/naohiro/Work/SATREPS/Shapefile/field_GIS/cihea/New_Test_Sites.shp'
+# Default values
+BLOCK_FNAM = '/home/naohiro/Work/SATREPS/Shapefile/cihea_testsite_200819/cihea_testsite_200819.shp'
+FIELD_FNAM = '/home/naohiro/Work/SATREPS/Shapefile/field_GIS/cihea/New_Test_Sites.shp'
+BLOCK_FIELD = 'Blok'
+BLOCK = 'None'
 
-r1 = shapefile.Reader(shp_fnam1)
+# Read options
+parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
+parser.add_option('-b','--block_fnam',default=BLOCK_FNAM,help='Block shape file name (%default)')
+parser.add_option('-f','--field_fnam',default=FIELD_FNAM,help='Field shape file name (%default)')
+parser.add_option('--block_field',default=BLOCK_FIELD,help='Field name of block in block_fnam (%default)')
+parser.add_option('-B','--block',default=BLOCK,help='Default block name (%default)')
+parser.add_option('--use_index',default=False,action='store_true',help='Use index instead of OBJECTID (%default)')
+parser.add_option('--use_objectid',default=False,action='store_true',help='Use OBJECTID for default block name (%default)')
+(opts,args) = parser.parse_args()
+
+r1 = shapefile.Reader(opts.block_fnam)
 blocks = []
 polygons = []
 for shaperec in r1.iterShapeRecords():
     rec = shaperec.record
     shp = shaperec.shape
-    blocks.append(rec[1])
     p1 = Polygon(shp.points)
     if not p1.is_valid:
         raise ValueError('Error, not valid >>> '+rec[1])
+    blocks.append(getattr(rec,opts.block_field))
     polygons.append(p1)
 
-r2 = shapefile.Reader(shp_fnam2)
-for shaperec in r2.iterShapeRecords():
+r2 = shapefile.Reader(opts.field_fnam)
+for iobj,shaperec in enumerate(r2.iterShapeRecords()):
     rec = shaperec.record
     shp = shaperec.shape
-    object_id = rec[0]
+    if opts.use_index:
+        object_id = iobj+1
+    else:
+        object_id = getattr(rec,'OBJECTID')
     p2 = Polygon(shp.points)
     block = None
     ratios = []
@@ -40,8 +57,12 @@ for shaperec in r2.iterShapeRecords():
     ratios = np.array(ratios)
     if block is None:
         i = np.argmax(ratios)
-        block = blocks[i]
         if ratios[i] < 1.0e-10:
-            block = 'None'
-        sys.stderr.write('Warning, error in finding block for {} ({}, {})\n'.format(object_id,block,ratios[i]))
-    sys.stdout.write('{:6d} {:>6s} {:.4e}\n'.format(object_id,block,1.0-ratios.max()))
+            if opts.use_objectid:
+                block = str(object_id)
+            else:
+                block = opts.block
+        else:
+            block = blocks[i]
+    sys.stdout.write('{:6d} {:>6s} {:.4e}\n'.format(object_id,block,ratios.max()))
+    #break
